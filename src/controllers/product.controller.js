cac41138fd7c3aff11e10ca99b35e5601b7e3f82
Isra QuirozZ <71298156+IsraQuirozZ@users.noreply.github.com>
2026-02-16 13:39:35 +0100

    Complete Products backend with pagination and toggle activation (#41)
    Co-authored-by: Israel Quiroz <israquirozz@MacBook-Pro-de-Israel.local>

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,14 +1,39 @@
 const productService = require("../services/product.service");
 
+// GET ALL PRODUCTS WITH PAGINATION, FILTERING AND SORTING
 const getAllProducts = async (req, res, next) => {
   try {
-    const products = await productService.getAllProducts();
-    res.json(products);
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const status = req.query.status || "active";
+
+    let where = {};
+
+    // Status filter
+    if (status == "active") where.active = true;
+    if (status == "inactive") where.active = false;
+
+    // Sort by name
+    const sort = req.query.sort || "name";
+    const order = req.query.order === "desc" ? "desc" : "asc";
+    let orderBy = [];
+    if (sort === "name") orderBy = [{ name: order }];
+
+    const [products, total] = await Promise.all([
+      productService.getAllProducts({ skip, take: limit, where, orderBy }),
+      productService.countProducts(where), // Count total products
+    ]);
+
+    const pages = Math.ceil(total / limit);
+    res.json({ data: products, page, pages, total });
   } catch (error) {
     next(error);
   }
 };
 
+// GET PRODUCT BY ID
 const getProductById = async (req, res, next) => {
   try {
     const id = Number(req.params.id);
@@ -19,6 +44,7 @@ const getProductById = async (req, res, next) => {
   }
 };
 
+// CREATE PRODUCT
 const createProduct = async (req, res, next) => {
   try {
     const product = await productService.createProduct(req.body);
@@ -43,7 +69,7 @@ const deleteProductById = async (req, res, next) => {
     const id = Number(req.params.id);
     const product = await productService.deleteProductById(id);
     res.json({
-      message: `Product --${product.name}-- with ID: ${product.id_product} successfully deleted`,
+      message: `Product: (${product.name}) ${product.active ? "activated" : "deactivated"} successfully`,
     });
   } catch (error) {
     next(error);
